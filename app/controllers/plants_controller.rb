3df900c86a53e309006cdbfd3b4e212fd6c9edd4
onyangoouma <jacksonouma04@gmail.com>
2022-09-27 12:21:24 +0300

    completed

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -12,10 +12,24 @@ class PlantsController < ApplicationController
     render json: plant
   end
 
-  # POST /plants
+  # POST /plants/:id
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  #PATCH /plants
+  def update
+    plant = find_plant
+    plant.update!(plant_params)
+    render json: plant
+  end
+
+  #DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    render json: {}
   end
 
   private
@@ -23,4 +37,9 @@ class PlantsController < ApplicationController
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+  
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
 end
